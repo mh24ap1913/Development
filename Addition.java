a699c5a0faef6033285511685a86a501eca3d144
mh24ap1913 <40120856+mh24ap1913@users.noreply.github.com>
2018-06-09 07:39:19 -0700

    Add files via upload
    addition of two number

---
+++ Addition.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Addition {
+	public static void main(String args[])
+	{
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Two number");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println("Result:"+c);
+		
+		
+		
+		
+		
+	}
+
+}
